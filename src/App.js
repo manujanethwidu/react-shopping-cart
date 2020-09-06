67a8435657f11223aa7f76bf3ff2bebefd45b9bc
unknown <manujanethwidu@gmail.com>
2020-09-06 05:36:23 +0530

    FilterComponentFinish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 import data from './data.json'
 import Products from './component/Products';
+import Filter from './component/Filter';
+import products from './component/Products';
 
 class App extends React.Component {
   constructor() {
@@ -12,6 +14,32 @@ class App extends React.Component {
       size: "",
       sort: ""
     }
+  }
+  sortProducts = (event) => {
+    const sort = event.target.value
+    this.setState(state => ({
+      sort,
+      products: this.state.products.slice().sort((a, b) => (
+        sort === "lowest" ?
+          ((a.price > b.price) ? 1 : -1) :
+          sort === "highest" ?
+            ((a.price < b.price) ? 1 : -1) :
+            ((a._id > b._id) ? 1 : -1)
+
+      ))
+    }))
+  }
+  filterProducts = (event) => {
+
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products })
+    } else {
+      this.setState({
+        size: event.target.value, products: data.products.filter((product) => product.availableSizes.indexOf(event.target.value) >= 0)
+      })
+    }
+
+
   }
   render() {
     return (
@@ -23,6 +51,13 @@ class App extends React.Component {
 
           <div className="content">
             <div className="main">
+              <Filter count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+
+              />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">
